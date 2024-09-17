8ce748f74a00b70e04e8358fe148f3623f3a2c42
Joshua Ozorji <ozorjijoshua@gmail.com>
2024-09-17 12:51:12 +0100

    login mg

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -36,13 +36,20 @@ const LoginPage = () => {
 		<main className='flex flex-col min-h-screen md:flex-row'>
 			<section className='hidden md:block md:w-[40%] bg-accent mx-auto min-h-screen justify-center'>
 				<div>
-					<img src={loginImage} alt='login image' className='object-contain ' />
+					<img
+						src={loginImage}
+						alt='login image'
+						className='object-contain '
+					/>
 				</div>
 
 				<div className='flex flex-col items-center '>
-					<h3 className='font-bold text-h2 font-bitter'>Welcome back</h3>
+					<h3 className='font-bold text-h2 font-bitter'>
+						Welcome back
+					</h3>
 					<p className='mb-6 text-sm'>
-						Start managing your finance faster and better
+						Start managing your finance
+						faster and better
 					</p>
 				</div>
 			</section>
@@ -50,15 +57,23 @@ const LoginPage = () => {
 			<section className='md:w-[60%] flex flex-col justify-center min-h-screen'>
 				<div className='block w-[80%] md:w-[50%] mx-auto space-y-4'>
 					<div>
-						<h2 className='font-bold text-h2 font-bitter'>Login</h2>
+						<h2 className='font-bold text-h2 font-bitter'>
+							Login
+						</h2>
 						<p className='font-light text-sec'>
-							Input your details to continue
+							Input your details to
+							continue
 						</p>
 					</div>
 
-					<form className='space-y-4' onSubmit={handleSubmit}>
+					<form
+						className='space-y-4'
+						onSubmit={handleSubmit}>
 						<div>
-							<label htmlFor='username'>Email or Username</label>
+							<label htmlFor='username'>
+								Email or
+								Username
+							</label>
 							<div className='label'>
 								<CiUser className='icon text-pry' />
 								<input
@@ -66,14 +81,20 @@ const LoginPage = () => {
 									name='identifier'
 									placeholder='email or username'
 									className='custom-input'
-									onChange={handleInputChange}
-									value={formData.identifier}
+									onChange={
+										handleInputChange
+									}
+									value={
+										formData.identifier
+									}
 								/>
 							</div>
 						</div>
 
 						<div>
-							<label htmlFor='password'>Password</label>
+							<label htmlFor='password'>
+								Password
+							</label>
 							<div className='label'>
 								<CiLock className='icon text-pry' />
 								<input
@@ -81,30 +102,44 @@ const LoginPage = () => {
 									name='password'
 									placeholder='password'
 									className='custom-input'
-									onChange={handleInputChange}
-									value={formData.password}
+									onChange={
+										handleInputChange
+									}
+									value={
+										formData.password
+									}
 								/>
 							</div>
 						</div>
 
 						<div className='text-h4 text-pry'>
-							<p className=''>Forgot password?</p>
+							<p className=''>
+								Forgot password?
+							</p>
 						</div>
 
 						<button className='button'>
-							<p>{isPending ? "Loading..." : "Login"}</p>
+							<p>
+								{isPending
+									? "Loading..."
+									: "Login"}
+							</p>
 						</button>
 
 						{isError && error && (
-							<p className='text-sm font-light text-center text-red-600 rounded-md'>
+							<p className='text-sm font-light text-center bg-red-600 rounded-md text-accent'>
 								{error.message}
 							</p>
 						)}
 					</form>
 
 					<div className='flex justify-center gap-2 pt-6 text-h4'>
-						<p className='text-sec'>Don't have an account?</p>{" "}
-						<Link className='text-pry hover:underline' to={"/signup"}>
+						<p className='text-sec'>
+							Don't have an account?
+						</p>{" "}
+						<Link
+							className='text-pry hover:underline'
+							to={"/signup"}>
 							Sign Up
 						</Link>
 					</div>
